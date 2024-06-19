01c6a99a3649ede75cb8ea52dcc716987951d57a
rajpootGovind <govindrajpoot12000@gmail.com>
2024-06-19 21:41:13 +0530

    RazorPay clone upto Features sections

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {fontFamily:{
+      mullish: ["Mullish", "sans-serif"]
+    },
+  colors:{
+    deepBlue:"#02042a"
+  }},
+    
+  },
+  plugins: [],
+}
+
